6ffd0b88ccfe19d1bc093d12ccbf0c4e1121078b
THE-DEEPDAS <deepdblm@gmail.com>
2025-02-23 11:04:12 +0530

    last time failed on prebuild

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,5 @@
 const { getDefaultConfig } = require("@expo/metro-config");
 
-const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
-config.resolver.unstable_enablePackageExports = true;
-config.resolver.unstable_conditionNames = ["require", "import"];
-config.resolver.sourceExts = ["js", "jsx", "json", "ts", "tsx"];
-config.resolver.assetExts = ["png", "jpg", "jpeg", "gif"];
-
-module.exports = config;
+module.exports = defaultConfig;
